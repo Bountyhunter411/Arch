47afa1743af2fa56eab785ad8aff8669f2f7d4b0
Bountyhunter411 <djr199712@gmail.com>
2022-04-09 11:57:31 +1200

    Added program installs

--- installArch.sh
+++ installArch.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# Functions
+
+	programs=(reflector)
+
+	applications=(zsh plasma konsole dolphin kate yay virt-manager bashtop bless burpsuite deluge deluge-gtk discord enum4linux filelight github-cli gnome-keyring gnu-netcat guvcview htop hydra john kdenlive linux-headers lutris metasploit mpv neofetch nmap ntfs-3g obs-studio obsidian openvpn s-tui signal-desktop speedtest-cli sqlmap steam tree unrar wget youtube-dl)
+
+	aur=(brave-bin gwe mangohud-git mangohud-common-git minecraft-launcher nerd-fonts-complete openrdb-git spotify steam-fonts via-bin xenia-bin)
 
 # Install dependencies
 
 {
 
 	pacman -Sy
-	programs=(reflector)
 
 	for program in "${programs[@]}"; do
 		if ! command -v "$program" > /dev/null 2>&1; then
@@ -14,6 +20,7 @@
 		fi
 	done
 }
+
 # Check connection and update mirrorlist
 
 {
@@ -30,8 +37,8 @@
 {
 
 	if (cat /etc/pacman.conf | grep blackarch >/dev/null)
-	then (echo "BlackArch Repo Exists" sleep 2)
-	else (echo "Adding BlackArch Repo" sleep 2)
+	then (echo "BlackArch Repo Exists" sleep 3)
+	else (echo "Adding BlackArch Repo" sleep 3)
 	       	curl -O https://blackarch.org/strap.sh
 		echo 8bfe5a569ba7d3b055077a4e5ceada94119cccef strap.sh | sha1sum -c
 		chmod +x strap.sh && ./strap.sh
@@ -41,5 +48,28 @@
 	if (cat /etc/pacman.conf | grep blackarch >/dev/null)
 	then (echo "BlackArch Repo Configured")
 	fi
+
+}
+
+# Install applications
+
+{
+
+	echo "Installing Applications" sleep 3
+	if (pacman -Sy "$applications" --noconfirm --needed)
+	then (echo "Applications Installed")
+	else (echo "Application Install Failed" sleep 3)
+
+
+}
+
+# Install Programs From AUR
+
+{
+
+	echo "Installing AUR Applications" sleep 3
+	if (yay -si -a --noconfirm "$aur")
+	then (echo "AUR Applications Installed" sleep 3)
+	else ( echo "AUR Applications Install Failed" sleep 3)
 
 }
